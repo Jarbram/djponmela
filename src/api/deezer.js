a743f58203691fc0fdd3adef5151e47b20e3effe
jarbram <huacchillocastillo@gmail.com>
2024-06-15 01:43:04 -0500

    update routes

--- src/api/deezer.js
+++ src/api/deezer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const searchSongs = async (query) => {
-  const response = await axios.get(`https://corsproxy.io/?https://api.deezer.com/search?q=${query}`);
+  const response = await axios.get(`https://crossorigin.me/?https://api.deezer.com/search?q=${query}`);
   return response.data.data;  
 };
