16b76b05a5a23b0a68ea5484568b20fd19222615
jarbram <huacchillocastillo@gmail.com>
2024-06-15 10:27:17 -0500

    vercel.json

--- src/api/deezer.js
+++ src/api/deezer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const searchSongs = async (query) => {
-  const response = await axios.get(`https://crossorigin.me/?https://api.deezer.com/search?q=${query}`);
+  const response = await axios.get(`/api/deezer/search?q=${query}`);
   return response.data.data;  
 };
