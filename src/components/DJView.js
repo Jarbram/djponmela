b2b4bc8009c65e41e3cc94d8f515aa00d3f139b2
jarbram <huacchillocastillo@gmail.com>
2024-07-05 18:33:31 -0500

    update the code

--- src/components/DJView.js
+++ src/components/DJView.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getDJRecords, deleteDJRecord, createSongRequest } from '../api/airtable';
-import { Container, Grid, Card, CardContent, Typography, Button, Box } from '@mui/material';
+import { Container, Grid, Card, CardContent, Typography, Button, Box, CircularProgress } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import LogoExtendido from '../assets/Logo DJ Ponla extendido (sin fondo).png'
 
@@ -8,6 +8,7 @@ const DJView = () => {
   const { djId } = useParams();
   const [songs, setSongs] = useState([]);
   const [isFiltered, setIsFiltered] = useState(false);
+  const [loadingSongs, setLoadingSongs] = useState({});
 
   useEffect(() => {
     const fetchSongs = async () => {
@@ -22,19 +23,18 @@ const DJView = () => {
   }, [djId]);
 
   const handleDelete = async (recordId) => {
+    setLoadingSongs(prevState => ({ ...prevState, [recordId]: true }));
+
     try {
-      // Obtener la canción que se va a eliminar
       const songToDelete = songs.find(song => song.id === recordId);
-
-      // Crear la canción en la tabla de canciones eliminadas
-      const newTableId = djId.replace(/^f|m$/g, ''); // Remover la 'f' inicial y 'm' final
+      const newTableId = djId.replace(/^f|m$/g, '');
       await createSongRequest(newTableId, songToDelete.fields['Song Name'], songToDelete.fields['Artist']);
-
-      // Eliminar la canción de la tabla original
       await deleteDJRecord(djId, recordId);
       setSongs(songs.filter(song => song.id !== recordId));
     } catch (error) {
       console.error('Error deleting song:', error);
+    } finally {
+      setLoadingSongs(prevState => ({ ...prevState, [recordId]: false }));
     }
   };
 
@@ -59,24 +59,24 @@ const DJView = () => {
       }}
     >
       <Container maxWidth="md">
-      <Box sx={{ textAlign: 'center', marginBottom: 0 }}>
-          <img src={LogoExtendido} alt='Logo DJPonla' style={{ width: '70%', height:'70%', borderRadius: 10 }} />
+        <Box sx={{ textAlign: 'center', marginBottom: 0 }}>
+          <img src={LogoExtendido} alt='Logo DJPonla' style={{ width: '70%', height: '70%', borderRadius: 10 }} />
         </Box>
         <Typography variant="h5" align="center" gutterBottom>
           La gente quiere escuchar:
         </Typography>
-        <Box textAlign="center" mb={4} >
-          <Button variant="contained" sx={{backgroundColor:'#54A772',margin:1, borderRadius:5}}   onClick={() => window.location.reload()}>
+        <Box textAlign="center" mb={4}>
+          <Button variant="contained" sx={{ backgroundColor: '#54A772', margin: 1, borderRadius: 5 }} onClick={() => window.location.reload()}>
             Actualizar Lista
           </Button>
-          <Button variant="contained" color="primary" sx={{border:'1px solid', margin:1, borderRadius:5 }} onClick={handleFilter}>
+          <Button variant="contained" color="primary" sx={{ border: '1px solid', margin: 1, borderRadius: 5 }} onClick={handleFilter}>
             {isFiltered ? 'Ordenar por Reciente' : 'Ordenar por Artista'}
           </Button>
         </Box>
         <Grid container spacing={3}>
           {songs.map((song, index) => (
             <Grid item xs={12} sm={6} md={4} key={index}>
-              <Card sx={{ backgroundColor: '#fff', color: '#000', borderRadius:5 }}>
+              <Card sx={{ backgroundColor: '#fff', color: '#000', borderRadius: 5 }}>
                 <CardContent>
                   <Typography variant="h6">
                     {song.fields['Song Name']}
@@ -85,8 +85,15 @@ const DJView = () => {
                     {song.fields['Artist']}
                   </Typography>
                   <Box mt={2}>
-                    <Button variant="contained" sx={{borderRadius:5}} color="error" fullWidth onClick={() => handleDelete(song.id)}>
-                      Ya la puse
+                    <Button
+                      variant="contained"
+                      sx={{ borderRadius: 5 }}
+                      color="error"
+                      fullWidth
+                      onClick={() => handleDelete(song.id)}
+                      disabled={loadingSongs[song.id]}
+                    >
+                      {loadingSongs[song.id] ? <CircularProgress size={24} /> : 'Ya la puse'}
                     </Button>
                   </Box>
                 </CardContent>
